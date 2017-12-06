30f5c20bb6c8fd8ea80ef676cd717716f35235ce
tomo1139 <tomo1139@gmail.com>
2017-12-06 21:53:14 +0900

    kotlin化

---
+++ EpoxyTest/app/src/main/java/develop/beta1139/epoxytest/MainActivity.kt
@@ -0,0 +1,12 @@
+package develop.beta1139.epoxytest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
